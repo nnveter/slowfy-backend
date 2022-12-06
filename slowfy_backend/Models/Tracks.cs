8dd223dd904b3fe988a23bd98d153b394a243a91
Илюша Илья <nnveter192@gmail.com>
2022-12-07 02:04:29 +0300

    5

--- slowfy_backend/Models/Tracks.cs
+++ slowfy_backend/Models/Tracks.cs
@@ -6,8 +6,6 @@ public class Tracks
     public string Author { get; set; }
     public string Title { get; set; }
     public string Duration { get; set; }
-    public string Source { get; set; }
     public bool E { get; set; }
-    public string Image { get; set; }
 
 }
